2fc6428aa4690633aa152e55623d0f10c825ff7b
dylanlima <dylanpachecocas@gmail.com>
2024-06-02 17:07:33 -0300

    Retirado flow do paymentTypeRepository e adicionada a chamada para a thread IO

--- app/src/main/java/com/example/organizafinancas/data/repository/PaymentTypeRepositoryImpl.kt
+++ app/src/main/java/com/example/organizafinancas/data/repository/PaymentTypeRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.example.organizafinancas.data.repository
 
 import com.example.organizafinancas.data.source.local.PaymentTypeDao
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class PaymentTypeRepositoryImpl @Inject constructor(
     private val dao: PaymentTypeDao
 ) : PaymentTypeRepository {
 
-    override fun fetchPaymentTypeFilters() = flow { emit(dao.getAll()) }
+    override fun fetchPaymentTypeFilters() = dao.getAll().flowOn(IO)
 }
