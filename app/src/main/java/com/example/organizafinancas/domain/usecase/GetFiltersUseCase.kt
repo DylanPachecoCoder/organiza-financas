c2dd6b41c03a0c93e46f5a754eb24316fb5a6d5b
dylanlima <dylanpachecocas@gmail.com>
2024-06-02 17:09:33 -0300

    Ajustada a logica do use case para retornar os filtros da forma correta

--- app/src/main/java/com/example/organizafinancas/domain/usecase/GetFiltersUseCase.kt
+++ app/src/main/java/com/example/organizafinancas/domain/usecase/GetFiltersUseCase.kt
@@ -2,7 +2,9 @@ package com.example.organizafinancas.domain.usecase
 
 import com.example.organizafinancas.data.repository.CategoryRepository
 import com.example.organizafinancas.data.repository.PaymentTypeRepository
+import com.example.organizafinancas.domain.model.Category
 import com.example.organizafinancas.domain.model.Filter
+import com.example.organizafinancas.domain.model.PaymentType
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -11,11 +13,19 @@ class GetFiltersUseCase @Inject constructor(
     private val categoryRepository: CategoryRepository,
 ) {
 
-    operator fun invoke() = flow {
-        val filters = mutableListOf<Filter>().apply {
-            paymentTypeRepository.fetchPaymentTypeFilters().collect { addAll(it) }
-            categoryRepository.fetchCategoryFilters().collect { addAll(it) }
+    suspend operator fun invoke() = flow {
+        val filters = mutableListOf<Filter>()
+        paymentTypeRepository.fetchPaymentTypeFilters().collect { newPaymentTypes ->
+            val oldPaymentTypes = filters.filterIsInstance<PaymentType>()
+            filters.removeAll(oldPaymentTypes)
+            filters.addAll(newPaymentTypes)
+            emit(filters)
         }
-        emit(filters)
+        categoryRepository.fetchCategoryFilters().collect { newCategories ->
+            val oldCategories = filters.filterIsInstance<Category>()
+            filters.removeAll(oldCategories)
+            filters.addAll(newCategories)
+            emit(filters)
+        }
     }
 }
